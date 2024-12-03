06bfa65afd8d0477128861b6e811510d749221c7
Your Name <165170008+jdorantes1987@users.noreply.github.com>
2024-12-03 08:38:05 -0400

    Implementación actualización manual de archivo historico de tasas BCV.

--- pages/page1.py
+++ pages/page1.py
@@ -3,12 +3,10 @@ import time
 import altair as alt 
 import streamlit as st
 from multiprocessing import Process, Queue
-from seaborn import heatmap, set_style, lineplot
-from matplotlib.pyplot import subplots, title, show, tight_layout, xticks
 from datetime import datetime, date
 from consulta_data import ClsData
 from navigation import make_sidebar
-from banco_central.bcv_estadisticas_tasas import actulizar_file_tasas
+from banco_central.bcv_estadisticas_tasas import actulizar_file_tasas, actulizar_file_tasas_manual
 from accesos.files_excel import datos_estadisticas_tasas
 
 st.set_page_config(page_title='DataPy: Inicio', 
@@ -27,21 +25,19 @@ def archivo_xlsx_bcv_actualizado():
     return archivo_actualizado
 
 def update_file(tasks_to_accomplish, tasks_that_are_done):
-      task = tasks_to_accomplish.get_nowait()
-      actulizar_file_tasas()
-      tasks_that_are_done.put(task)
-      time.sleep(.5)
+      update_f = actulizar_file_tasas()
+      if update_f:
+        tasks_that_are_done.put(True)
+      else:
+        tasks_that_are_done.put(False)
 
-def actualizar_tasa_bcv():
+def update_tasa_bcv():
     tasks_to_accomplish = Queue()
     tasks_that_are_done = Queue()
-    tasks_to_accomplish.put("Tasa de cambio actualizada!")
     p = Process(target=update_file,args=(tasks_to_accomplish, tasks_that_are_done))
     p.start()
     p.join()
-    st.info(tasks_that_are_done.get())
-    time.sleep(1)
-    st.rerun()
+    return tasks_that_are_done.get()
           
 
 def local_css(file_name):
@@ -49,11 +45,13 @@ def local_css(file_name):
             st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
+def tasa_manual(fecha, valor):
+   actulizar_file_tasas_manual(fecha=fecha, valor_tasa=valor)
+
 if __name__ == '__main__':
     odata = ClsData(data_base=os.getenv("DB_NAME_IZQUIERDA_PROFIT"))
     new_cod = odata.generar_cod_cliente()
     date = odata.get_fecha_tasa_bcv_dia().date()
-
     table_scorecard = """
     <div class="ui five small statistics">
       <div class="grey statistic">
@@ -82,7 +80,17 @@ if __name__ == '__main__':
     local_css("style.css")
     make_sidebar()
     if not archivo_xlsx_bcv_actualizado():
-       actualizar_tasa_bcv()
+      if update_tasa_bcv():
+        st.info('Tasa BCV actualizada!')
+        time.sleep(0.5)
+      else:
+        st.warning('No se pudo actualizar el archivo de histórico de tasas BCV')
+        fecha = st.date_input("fecha de la tasa", disabled=True).strftime('%Y%m%d') # Convierte la fecha a YYYYMMDD
+        valor = st.number_input('Ingrese el valor de la tasa', format="%.5f")
+        if st.button("Deseas actualizar la tasa de forma manual?",  on_click=tasa_manual, args=(fecha, valor)):
+          st.info('Tasa BCV actualizada!')
+          st.rerun()
+    
 
 with st.expander("📊  Evolución tasa BCV"):
      var_tasa = datos_estadisticas_tasas()
