1aa4584977224c9f7a764e9e60a120635bf715a8
jdorantes <165170008+jdorantes1987@users.noreply.github.com>
2025-02-06 13:57:58 -0400

    Actualización de módulo con flake8

--- mikrowisp/controller/gestor_clientes.py
+++ mikrowisp/controller/gestor_clientes.py
@@ -1,7 +1,9 @@
 from datetime import datetime
-from mikrowisp.controller.clientes import Gestor
-from gestion_user.usuarios import ClsUsuarios
+
 from empresa import ClsEmpresa
+from gestion_user.usuarios import ClsUsuarios
+from mikrowisp.controller.clientes import Gestor
+
 
 class GestionarClientes(Gestor):
     @staticmethod
@@ -9,44 +11,47 @@ class GestionarClientes(Gestor):
         def wrapper(self, *args, **kwargs):
             today = datetime.now()
             modulo = ClsEmpresa.modulo()
-            print(f'Módulo: {modulo} Fecha: {today} Usuario: {ClsUsuarios.nombre()} running: {method.__name__}')
+            print(
+                f"Módulo: {modulo} Fecha: {today} Usuario: {ClsUsuarios.nombre()} running: {method.__name__}"
+            )
             return method(self, *args, **kwargs)  # Ojo llamada!
+
         return wrapper
-    
+
     def __init__(self, data_base):
-            super().__init__(data_base)
+        super().__init__(data_base)
 
     def clientes_mikrowisp(self):
-          return super().clientes_mikrowisp()
-    
+        return super().clientes_mikrowisp()
+
     def clientes_mikrowisp_aviso_user(self):
-          return super().clientes_mikrowisp_aviso_user()
-      
+        return super().clientes_mikrowisp_aviso_user()
+
     def clientes_profit(self):
-          return super().clientes_profit()
-    
+        return super().clientes_profit()
+
     @audit
     def add_clientes_en_mikrowisp(self):
-          return super().add_clientes_en_mikrowisp()
+        return super().add_clientes_en_mikrowisp()
 
-    @audit  
+    @audit
     def add_notificaciones(self):
-          return super().add_notificaciones()
-      
+        return super().add_notificaciones()
+
     def datos_clientes_por_sinc_profit_mikrowisp(self):
-          return super().datos_clientes_por_sinc_profit_mikrowisp()
-    
-    @audit  
+        return super().datos_clientes_por_sinc_profit_mikrowisp()
+
+    @audit
     def sinc_datos_clientes_profit_mikrowisp(self):
-          return super().sinc_datos_clientes_profit_mikrowisp()
-    
+        return super().sinc_datos_clientes_profit_mikrowisp()
+
     def datos_clientes_nodo_por_sinc_mikrowisp_profit(self):
-          return super().datos_clientes_nodo_por_sinc_mikrowisp_profit()
-    
+        return super().datos_clientes_nodo_por_sinc_mikrowisp_profit()
+
     @audit
     def sinc_datos_clientes_nodos(self):
-          return super().sinc_datos_clientes_nodos()
-    
+        return super().sinc_datos_clientes_nodos()
+
     @audit
     def ultimo_plan_facturado(self):
-          return super().ultimo_plan_facturado()
+        return super().ultimo_plan_facturado()
