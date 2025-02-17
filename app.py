afebe92f28dd44d1bd1bdc7e7ab32646fa6bf6df
jdorantes <165170008+jdorantes1987@users.noreply.github.com>
2025-02-17 08:50:54 -0400

    Se optimizó el login para validar usuario presionando la tecla enter.

--- app.py
+++ app.py
@@ -23,11 +23,22 @@ def roles():
 
 st.title("Inicio de sesión")
 st.write("Por favor ingrese su usuario y clave.")
-username = st.text_input("usuario")
-password = st.text_input("clave", type="password")
 
-if st.button("Log in", type="primary"):
-    if is_valid := aut_user(username, password):
+
+if "stage" not in st.session_state:
+    st.session_state.stage = 0
+
+
+def set_stage(i):
+    st.session_state.stage = i
+
+
+username = st.text_input("usuario")
+password = st.text_input("clave", type="password", on_change=set_stage, args=[1])
+
+
+def login():
+    if aut_user(username, password):
         date = datetime.now()
         print(f"{date} Usuario {username} ha iniciado sesión.")
         st.session_state.logged_in = True
@@ -35,10 +46,19 @@ if st.button("Log in", type="primary"):
         st.cache_data.clear()
         st.session_state["refrescar_facturacion"] = False
         sleep(0.5)
-        roles = roles()
-        modulo = "Izquierda" if roles.get("Izquierda", 0)[1] == 1 else "Derecha"
+        user_roles = roles()
+        modulo = "Izquierda" if user_roles.get("Izquierda", 0)[1] == 1 else "Derecha"
         st.session_state.modulo = modulo
         ClsEmpresa(modulo, False)
+        set_stage(0)
         st.switch_page(MENU_INICIO)
     else:
         st.error("usuario o clave incorrecta")
+
+
+if st.session_state.stage == 1:
+    login()
+
+
+if st.button("Log in", type="primary"):
+    login()
